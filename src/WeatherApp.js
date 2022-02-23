ae5d11826cd3a7b03d4037d2336606e8d17e378f
Shaniesha John <shaniesha.john.94@gmail.com>
2022-02-22 21:49:38 -0500

    Removed the exclude part

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -169,7 +169,7 @@ function displayWeatherForecast(response) {
 pin the coordinates and then display the forecast */
 function getForecast(coordinates) {
   let apiKey = "ef67b1869cadffe12a36cdbb72626863";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={part}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherForecast);
 }
 
