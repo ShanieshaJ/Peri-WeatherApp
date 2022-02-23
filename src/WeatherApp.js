b034ac4f187f278337e106e75615eb5e61404963
Shaniesha John <shaniesha.john.94@gmail.com>
2022-02-23 01:51:45 -0500

    Removed and reentered the img src of the icon

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -146,9 +146,9 @@ function displayWeatherForecast(response) {
         <div class="weather-forecast-date">
           ${formatDay(forecastDay.dt)}
         </div>
-        <img src="http://openweathermap.org/img/wn/${
+        <img src= "http://openweathermap.org/img/wn/${
           forecastDay.weather[0].icon
-        }@2px.png" alt="icons" width="42"/>
+        }@2x.png" alt="" width="100" />
           <div class="weather-forecast-temperatures">
             <span class="forecast-temperature-max">
             <strong>${Math.round(forecastDay.temp.max)}°</strong>
