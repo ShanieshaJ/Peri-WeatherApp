9fdefe6453b18d3c0916ab99d69680f69328f2e4
Shaniesha John <shaniesha.john.94@gmail.com>
2022-02-23 22:02:51 -0500

    Added sunrise, sunset text & unit conversion edit

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -41,6 +41,38 @@ function displayCurrentWeatherReport(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 
+  //To display sunrise and sunset
+  let sunriseElement = document.querySelector("#sunriseInfo");
+  let sunsetElement = document.querySelector("#sunsetInfo");
+  let sunriseTime = new Date(response.data.sys.sunrise * 1000);
+  let sunsetTime = new Date(response.data.sys.sunset * 1000);
+
+  let currentSunriseHours = sunriseTime.getHours();
+  let currentSunriseMinutes = sunrise.getMinutes();
+
+  if (currentSunriseHours < 10) {
+    currentSunriseHours = `0${currentSunriseHours}`;
+  }
+
+  if (currentSunriseMinutes < 10) {
+    currentSunriseMinutes = `0${currentSunriseMinutes}`;
+  }
+
+  sunriseElement.innerHTML = `${currentSunriseHours}:${currentSunriseMinutes}`;
+
+  let currentSunsetHours = sunsetTime.getHours();
+  let currentSunsetMinutes = sunset.getMinutes();
+
+  if (currentSunsetHours < 10) {
+    currentSunsetHours = `0${currentSunsetHours}`;
+  }
+
+  if (currentSunsetMinutes < 10) {
+    currentSunsetMinutes = `0${currentSunsetMinutes}`;
+  }
+
+  sunsetElement.innerHTML = `${currentSunsetHours}:${currentSunsetMinutes}`;
+
   getForecast(response.data.coord);
 }
 
@@ -110,6 +142,34 @@ function showConversionCelsius(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   currentWindSpeed.innerHTML = Math.round(windKilo);
   currentWindUnit.innerHTML = "km/h";
+
+  let forecastInfo = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  forecastInfo.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+        <div class="weather-forecast-date">
+          ${formatDay(forecastDay.dt)}
+        </div>
+        <img src= "http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt="" id="weather-forecast-icons" width="100" />
+          <div class="weather-forecast-temperatures">
+            <span class="forecast-temperature-max">
+            <strong>${Math.round(forecastDay.temp.max)}°</strong>
+            </span> /
+            <span class="forecast-temperature-min">
+            ${Math.round(forecastDay.temp.min)}°
+            </span>
+          </div>
+        </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 /* Seventh function change the conversion to fahrenheit by clicking */
@@ -122,7 +182,36 @@ function showConversionFahrenheit(event) {
   temperatureElement.innerHTML = Math.round(fahrenheitConversion);
   currentWindSpeed.innerHTML = Math.round(windKilo / 1.609344);
   currentWindUnit.innerHTML = "mph";
+
+  let forecastInfo = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  forecastInfo.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+        <div class="weather-forecast-date">
+          ${formatDay(forecastDay.dt)}
+        </div>
+        <img src= "http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt="" id="weather-forecast-icons" width="100" />
+          <div class="weather-forecast-temperatures">
+            <span class="forecast-temperature-max">
+            <strong>${Math.round(forecastDay.temp.max * 9) / 5 + 32}°</strong>
+            </span> /
+            <span class="forecast-temperature-min">
+            ${Math.round(forecastDay.temp.min * 9) / 5 + 32}°
+            </span>
+          </div>
+        </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
+
 /* Created an variables to set temperature in celsius, wind units to null */
 let celsiusTemperature = null;
 let windKilo = null;
